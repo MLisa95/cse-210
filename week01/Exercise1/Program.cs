1795408081a424a77beb953f32f6fa667a2034b6
MLisa95 <lgasa@byupathway.edu>
2025-05-17 22:09:32 +0200

    conditions practice

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your first name? >");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("What is your surname?");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine($"My name is {surname}, {name} {surname}.");
     }
 }
--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Enter a random number. ");
+        float number_one = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter a second random number. ");
+        float number_two = float.Parse(Console.ReadLine());
+
+        if (number_one > number_two)
+        {
+            Console.WriteLine("Your first number is greater than your second number.");
+        }
+        else
+        {
+            Console.WriteLine("Your second number is greater than you first number. ");
+        }
     }
 }
