98832cafe7688b4085ae2aa80618a1e8e324d18c
MLisa95 <lgasa@byupathway.edu>
2025-06-17 09:08:01 +0200

    shape area calculation

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("yellow", 7.2);
+        Rectangle rectangle = new Rectangle("red", 4.4, 7.9);
+        Circle circle = new Circle("blue", 3.41);
+
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():0.00}\n");
+        }
     }
 }
